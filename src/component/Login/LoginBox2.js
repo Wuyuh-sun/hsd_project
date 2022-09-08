9fcb2890ecc8436044c1e422950fcc428f4313ed
yunha woo <wjh0970@gmail.com>
2022-09-08 16:08:55 +0900

    pj_6days_3_commit

--- src/component/Login/LoginBox2.js
+++ src/component/Login/LoginBox2.js
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "../../css/LoginCSS/LoginWrap.module.css";
+
 export default function LoginBox2(){
   return(
     <>
+    <div className={styles.box2}>
+      <div className={styles.box2_img}></div>
+      <div className={styles.join_intro}>
+        <div>한솥 회원이 아니신가요?</div>
+        <br />
+        <div>회원이 되시면 한솥 도시락의 다양한 정보와</div>
+        <div>이벤트를 만나보실 수 있습니다.</div>
+        <Link to="#">회원가입</Link>
+      </div>
+    </div>
     </>
   )
 }
